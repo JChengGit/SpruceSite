3618129f3764a17579f6aa35aa4603a616784c46
JChengGit <JChengGit@163.com>
2023-05-07 23:52:24 +0800

    add user login

--- router/user.py
+++ router/user.py
@@ -19,3 +19,12 @@ async def create_user(item: UserIn, db: Session = Depends(get_db)):
     db.add(user)
     db.commit()
     return 1
+
+
+@router.post("/login")
+async def login(item: UserLogin, response: Response, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.name == item.name, User.password == item.password).first()
+    if not user:
+        return "login failed"
+    response.set_cookie(key="ticket", value=str(uuid.uuid4()))
+    return "login succeed"
